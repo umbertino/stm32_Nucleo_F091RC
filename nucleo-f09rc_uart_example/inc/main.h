9240cc3b12f97092ffbd79a0218ffc215b528d3b
umberto <umberto@urano.fritz.box>
2016-10-25 22:40:00 +0200

    update

---
+++ nucleo-f09rc_uart_example/inc/main.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: 25.10.2016
+ *      Author: umberto
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+
+
+
+#endif /* MAIN_H_ */
--- nucleo-f09rc_uart_example/src/main.c
+++ nucleo-f09rc_uart_example/src/main.c
@@ -31,8 +31,7 @@
   ******************************************************************************
   */
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f0xx_hal.h"
-#include "stm32f0xx_hal_tim.h"
+#include "main.h"
 #include "mxconstants.h"
 #include <string.h>
 
@@ -42,7 +41,12 @@
 
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart2;
-TIM_HandleTypeDef htim3;
+/* TIM handle declaration */
+TIM_HandleTypeDef    TimHandle;
+
+/* Prescaler declaration */
+uint32_t uwPrescalerValue = 0;
+
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
@@ -102,8 +106,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   InitializeTimer();
-  HAL_TIM_Base_Start_IT(&htim3);
-  //BSP_LED_Init(LED2);
+  BSP_LED_Init(LED2);
   //BSP_PB_Init(BUTTON_USER, BUTTON_MODE_GPIO);
 
 
@@ -119,13 +122,13 @@ int main(void)
 
   while (1)
   {
-	  printf("Hello Printf %d\r\n", cnt % 10);
+	  printf("B1-State: %d | Hello %02d\n\r", HAL_GPIO_ReadPin(GPIOC, B1_Pin), cnt % 10);
 	  //HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), 0xFFFF);
 	  //printf("B1-State: %lu\r\n", BSP_PB_GetState(BUTTON_USER));
-	  printf("B1-State: %d\r\n", HAL_GPIO_ReadPin(GPIOC, B1_Pin));
+	  //printf("B1-State: %d\r\n", HAL_GPIO_ReadPin(GPIOC, B1_Pin));
 	  HAL_Delay(500);
 	  //BSP_LED_Toggle(LED2);
-	  HAL_GPIO_TogglePin(GPIOA, LD2_Pin);
+	  //HAL_GPIO_TogglePin(GPIOA, LD2_Pin);
 	  cnt++;
   /* USER CODE END WHILE */
 
@@ -138,45 +141,6 @@ int main(void)
 
 /** System Clock Configuration
 */
-void SystemClock_Config(void)
-{
-
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_PeriphCLKInitTypeDef PeriphClkInit;
-
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
-  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
-  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-
-  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-
-  /* SysTick_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
-}
 
 /* USART2 init function */
 static void MX_USART2_UART_Init(void)
@@ -201,40 +165,50 @@ static void MX_USART2_UART_Init(void)
 
 void InitializeTimer(void)
 {
-    TIM_ClockConfigTypeDef sClockSourceConfig;
-    TIM_MasterConfigTypeDef sMasterConfig;
+	/* Compute the prescaler value to have TIMx counter clock equal to 10000 Hz */
+	  uwPrescalerValue = (uint32_t)(SystemCoreClock / 10000) - 1;
 
-    htim3.Instance = TIM3;
-    htim3.Init.Prescaler = 48000;
-    htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim3.Init.Period = 500;
-    htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-    if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
-    {
-      Error_Handler();
-    }
+	  /* Set TIMx instance */
+	  TimHandle.Instance = TIMx;
 
-    sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-    if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
-    {
-      Error_Handler();
-    }
+	  /* Initialize TIMx peripheral as follows:
+	       + Period = 10000 - 1
+	       + Prescaler = (SystemCoreClock/10000) - 1
+	       + ClockDivision = 0
+	       + Counter direction = Up
+	  */
+	  TimHandle.Init.Period            = 10000 - 1;
+	  TimHandle.Init.Prescaler         = uwPrescalerValue;
+	  TimHandle.Init.ClockDivision     = 0;
+	  TimHandle.Init.CounterMode       = TIM_COUNTERMODE_UP;
+	  TimHandle.Init.RepetitionCounter = 0;
+	  if (HAL_TIM_Base_Init(&TimHandle) != HAL_OK)
+	  {
+	    /* Initialization Error */
+	    Error_Handler();
+	  }
 
-    sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-    if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
-    {
-      Error_Handler();
-    }
+	  /*##-2- Start the TIM Base generation in interrupt mode ####################*/
+	  /* Start Channel1 */
+	  if (HAL_TIM_Base_Start_IT(&TimHandle) != HAL_OK)
+	  {
+	    /* Starting Error */
+	    Error_Handler();
+	  }
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-  if (htim->Instance==TIM3)
-  {
-    printf("TIM3 fired\r\n");
-  }
+  BSP_LED_Toggle(LD2_Pin);
 }
+
+//void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+//{
+//  if (htim->Instance==TIM3)
+//  {
+//    printf("TIM3 fired\r\n");
+//  }
+//}
 
 /** Configure pins as
         * Analog
@@ -274,6 +248,33 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  
+  /* Select HSI48 Oscillator as PLL source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI48;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Select PLL as system clock source and configure the HCLK and PCLK1 clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+}
 
 /**
   * @brief  This function is executed in case of error occurrence.
